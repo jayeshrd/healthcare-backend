8481c7d2160a4abba286270b23f5282eb0b084e0
saurabh-godigital <saurabh.yadav@zenithgodigital.com>
2023-11-28 18:18:07 +0530

    fixed otp template

--- src/models/otp.model.ts
+++ src/models/otp.model.ts
@@ -40,10 +40,22 @@ export const sendOtpToEmail = async (email: string, otp: number) => {
   try {
     const mailResponse = await mailSender(
       email,
-      "Verification Email",
+      "Accufly - OTP",
       `
-      <p>Here is your OTP code: ${otp}</p>
-      <b>NOTE:</b> Only valid for 5 min
+      <div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2">
+        <div style="margin:50px auto;width:70%;padding:20px 0">
+          <div style="border-bottom:1px solid #eee">
+          </div>
+          <p style="font-size:1.1em">Hi,</p>
+          <p>Copy the following OTP to complete your Sign Up procedures.<br> <b>OTP is valid for 5 minutes only</b></p>
+          <h2 style="background: #00466a;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;">${otp}</h2>
+          <p style="font-size:0.9em;">Regards,<br />Accufly</p>
+          <hr style="border:none;border-top:1px solid #eee" />
+          <div style="float:right;padding:8px 0;color:#aaa;font-size:0.8em;line-height:1;font-weight:300">
+            <p>Accufly</p>
+          </div>
+        </div>
+      </div>
       `
     );
     console.log("Email sent successfully: ", mailResponse);
